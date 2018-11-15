d2742c24ff944d9962d740ba532de5b0c88cb53f
Chau <ctran2428@gmail.com>
2018-11-15 12:47:19 -0600

    start of 06

--- ng-coders-tokyo/src/app/app.component.ts
+++ ng-coders-tokyo/src/app/app.component.ts
@@ -3,12 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Welcome to Coders.Tokyo!</h1>
-    <app-hello></app-hello>
-    <app-hi></app-hi>
+    <h1 [ngClass]="{
+      'with-border': withBorder,
+      'other-class': true
+    }">{{ title }}</h1>
+    <img [src]="imageSrc">
   `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-coders-tokyo';
+  title = 'Coders.Tokyo Updated';
+  imageSrc = 'https://picsum.photos/200';
+
+  textColor = 'tomato';
+  backgroundColor = 'black';
+
+  styleObj = {color: this.textColor, background: this.backgroundColor};
+
+  withBorder = true;
 }
+// DataBinding
+// 1. PropertyBinding
+// 2. EventBinding
