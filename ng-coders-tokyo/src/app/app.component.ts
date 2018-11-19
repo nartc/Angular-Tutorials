ac0f57aa0a10411c7a097425039af222ec8966e3
Chau <ctran2428@gmail.com>
2018-11-18 20:47:25 -0600

    Life-cycle hooks prep

--- ng-coders-tokyo/src/app/app.component.ts
+++ ng-coders-tokyo/src/app/app.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  OnDestroy,
+  AfterViewInit,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterContentChecked,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -15,12 +25,35 @@ import { Component } from '@angular/core';
   `,
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent
+  implements OnInit, OnChanges, OnDestroy, AfterViewInit, AfterContentInit, AfterViewChecked, AfterContentChecked {
   title = 'Coders.Tokyo Updated';
   imageSrc = 'https://picsum.photos/200';
 
   textColor = 'tomato';
   withBorder = true;
+
+  ngOnInit(): void {
+    console.log('OnInit ran');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('OnChanges ran', { changes });
+  }
+  ngOnDestroy(): void {
+    console.log('OnDestroy ran');
+  }
+  ngAfterViewInit(): void {
+    console.log('AfterViewInit ran');
+  }
+  ngAfterContentInit(): void {
+    console.log('AfterContentInit ran');
+  }
+  ngAfterViewChecked(): void {
+    console.log('AfterViewChecked ran');
+  }
+  ngAfterContentChecked(): void {
+    console.log('AfterContentChecked ran');
+  }
 
   onTextMouseOver() {
     this.textColor = 'dodgerblue';
--- ng-coders-tokyo/src/app/components/hello/hello.component.ts
+++ ng-coders-tokyo/src/app/components/hello/hello.component.ts
@@ -1,9 +1,51 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  SimpleChanges,
+  OnInit,
+  OnChanges,
+  OnDestroy,
+  AfterViewInit,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterContentChecked,
+  Input,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 
 @Component({
   selector: 'app-hello',
   template: `
     <h2>Hello from hello component</h2>
-  `
+    <p>{{ text }} from parent</p>
+
+    <div *ngFor="let item of (object | keyvalue)">{{ item.key }}:{{ item.value }}</div>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HelloComponent {}
+export class HelloComponent
+  implements OnInit, OnChanges, OnDestroy, AfterViewInit, AfterContentInit, AfterViewChecked, AfterContentChecked {
+  @Input() text: string;
+  @Input() object: any;
+
+  ngOnInit(): void {
+    console.log('Child OnInit ran');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Child OnChanges ran', { changes });
+  }
+  ngOnDestroy(): void {
+    console.log('Child OnDestroy ran');
+  }
+  ngAfterViewInit(): void {
+    console.log('Child AfterViewInit ran');
+  }
+  ngAfterContentInit(): void {
+    console.log('Child AfterContentInit ran');
+  }
+  ngAfterViewChecked(): void {
+    console.log('Child AfterViewChecked ran');
+  }
+  ngAfterContentChecked(): void {
+    console.log('Child AfterContentChecked ran');
+  }
+}
