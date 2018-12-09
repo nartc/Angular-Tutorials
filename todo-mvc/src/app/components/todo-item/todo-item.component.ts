3d6998cbb1800a4307a456aa64f694d8bfb1a1e1
Chau <ctran2428@gmail.com>
2018-12-09 14:58:39 -0600

    part4

--- todo-mvc/src/app/components/todo-item/todo-item.component.ts
+++ todo-mvc/src/app/components/todo-item/todo-item.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoItemComponent implements OnInit {
 
+  isHovered = false;
+  isEditing = false;
+
   constructor() { }
 
   ngOnInit() {
